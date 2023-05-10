14d8f98e53cfda77e9f34134b3b0955aa03b789e
Dan Vyenielo <dvyenielo@pm.me>
2023-05-10 01:26:09 -0700

    Basic block model.

--- tetrit/src/tetrit/blocks/block.hpp
+++ tetrit/src/tetrit/blocks/block.hpp
@@ -10,10 +10,34 @@ namespace blocks {
 
 class block_t
 {
-    using point_t = Eigen::Matrix<int32_t, 2, 1>;
+public:
+    using value_t = int64_t;
+    using points_t = Eigen::Matrix<value_t, Eigen::Dynamic, 2>;
+    points_t points;
+
+private:
+    /**
+     * @warning eigen implements *= operator expansion on the rhs (eg: A *= B
+     * is equivalent to A = A * B). This is conceptually inconvenient for
+     * rotation operations since it requires a transpose operation to maintain
+     * consistency with traditional mathematical notation where matrix A is
+     * rotated by matrix R using A = R * A = A * R^T.
+     * https://eigen.tuxfamily.org/dox/group__TutorialMatrixArithmetic.html
+     */
+    using rotation_t = Eigen::Matrix<value_t, 2, 2>;
 
 public:
-    int val{ 0 };
+    /**
+     * @brief rotates this object 90 degrees clockwise.
+     * @note rotation matrix is manually transposed to avoid runtime overhead.
+     */
+    void rotate_cw() { points *= rotation_t{ { 0, -1 }, { 1, 0 } }; }
+
+    /**
+     * @brief rotates this object 90 degrees counter-clockwise.
+     * @note rotation matrix is manually transposed to avoid runtime overhead.
+     */
+    void rotate_ccw() { points *= rotation_t{ { 0, 1 }, { -1, 0 } }; }
 };
 
 } // namespace blocks
--- tetrit/test/src/test_blocks_block.cpp
+++ tetrit/test/src/test_blocks_block.cpp
@@ -1,9 +1,51 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
+
 #include "tetrit/blocks/block.hpp"
 
 TEST_CASE("test_tetrit_blocks_block_default")
 {
     tetrit::blocks::block_t b;
-    REQUIRE(b.val == 0);
+    REQUIRE(b.points.cols() == 2);
+    REQUIRE(b.points.rows() == 0);
+}
+
+TEST_CASE("test_tetrit_blocks_block_points_assign")
+{
+    tetrit::blocks::block_t b;
+    b.points = tetrit::blocks::block_t::points_t{ { 0, 2 },
+                                                  { 2, 0 },
+                                                  { 0, -2 },
+                                                  { -2, 0 } };
+}
+
+TEST_CASE("test_tetrit_blocks_block_points_rotate_cw")
+{
+    tetrit::blocks::block_t b;
+    b.points = tetrit::blocks::block_t::points_t{ { 0, 2 },
+                                                  { 2, 0 },
+                                                  { 0, -2 },
+                                                  { -2, 0 } };
+    b.rotate_cw();
+    REQUIRE(b.points
+            == tetrit::blocks::block_t::points_t{ { 2, 0 },
+                                                  { 0, -2 },
+                                                  { -2, 0 },
+                                                  { 0, 2 } });
+}
+
+TEST_CASE("test_tetrit_blocks_block_points_rotate_ccw")
+{
+    tetrit::blocks::block_t b;
+    b.points = tetrit::blocks::block_t::points_t{ { 0, 2 },
+                                                  { 2, 0 },
+                                                  { 0, -2 },
+                                                  { -2, 0 } };
+    b.rotate_ccw();
+    REQUIRE(b.points
+            == tetrit::blocks::block_t::points_t{ { -2, 0 },
+                                                  { 0, 2 },
+                                                  { 2, 0 },
+                                                  { 0, -2 } });
 }
