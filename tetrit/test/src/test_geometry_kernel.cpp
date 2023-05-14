655872b7fa63d0d044435895f019bf31e0abb99e
Dan Vyenielo <dvyenielo@pm.me>
2023-05-14 15:00:07 -0700

    Propigate portable type to tests.

--- tetrit/test/src/test_geometry_kernel.cpp
+++ tetrit/test/src/test_geometry_kernel.cpp
@@ -7,15 +7,15 @@
 TEST_CASE("test_tetrit_geometry_kernel_value_t")
 {
     using kernel_t = tetrit::geometry::kernel_t;
-    REQUIRE(std::is_same<kernel_t::value_t, std::int32_t>::value);
-    REQUIRE(sizeof(kernel_t::value_t) == 4);
+    REQUIRE(std::is_same<kernel_t::value_t, std::int_fast32_t>::value);
+    REQUIRE(sizeof(kernel_t::value_t) >= 4);
 }
 
 TEST_CASE("test_tetrit_geometry_kernel_point_t")
 {
     using kernel_t = tetrit::geometry::kernel_t;
     REQUIRE(std::is_same<kernel_t::point_t,
-                         Eigen::Matrix<std::int32_t, 1, 2>>::value);
+                         Eigen::Matrix<std::int_fast32_t, 1, 2>>::value);
     kernel_t::point_t p;
     REQUIRE(p.NumDimensions == 1);
     REQUIRE(p.rows() == 1);
@@ -26,7 +26,8 @@ TEST_CASE("test_tetrit_geometry_kernel_points_t")
 {
     using kernel_t = tetrit::geometry::kernel_t;
     using points_t = kernel_t::points_t<2>;
-    REQUIRE(std::is_same<points_t, Eigen::Matrix<std::int32_t, 2, 2>>::value);
+    REQUIRE(
+        std::is_same<points_t, Eigen::Matrix<std::int_fast32_t, 2, 2>>::value);
     points_t ps;
     REQUIRE(ps.NumDimensions == 2);
     REQUIRE(!ps.IsRowMajor);
