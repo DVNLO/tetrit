f44afdc7d865e8b3a054195e086c516dd8f2d473
Dan Vyenielo <dvyenielo@pm.me>
2023-05-14 12:35:38 -0700

    Fortify kernel tests.

--- tetrit/test/src/test_geometry_kernel.cpp
+++ tetrit/test/src/test_geometry_kernel.cpp
@@ -8,6 +8,7 @@ TEST_CASE("test_tetrit_geometry_kernel_value_t")
 {
     using kernel_t = tetrit::geometry::kernel_t;
     REQUIRE(std::is_same<kernel_t::value_t, std::int32_t>::value);
+    REQUIRE(sizeof(kernel_t::value_t) == 4);
 }
 
 TEST_CASE("test_tetrit_geometry_kernel_point_t")
@@ -15,6 +16,9 @@ TEST_CASE("test_tetrit_geometry_kernel_point_t")
     using kernel_t = tetrit::geometry::kernel_t;
     REQUIRE(std::is_same<kernel_t::point_t,
                          Eigen::Matrix<std::int32_t, 1, 2>>::value);
+    kernel_t::point_t p;
+    REQUIRE(p.rows() == 1);
+    REQUIRE(p.cols() == 2);
 }
 
 TEST_CASE("test_tetrit_geometry_kernel_points_t")
@@ -22,4 +26,8 @@ TEST_CASE("test_tetrit_geometry_kernel_points_t")
     using kernel_t = tetrit::geometry::kernel_t;
     REQUIRE(std::is_same<kernel_t::points_t,
                          Eigen::Matrix<std::int32_t, -1, 2>>::value);
+    kernel_t::points_t ps;
+    REQUIRE(!ps.IsRowMajor);
+    REQUIRE(ps.rows() == 0);
+    REQUIRE(ps.cols() == 2);
 }
